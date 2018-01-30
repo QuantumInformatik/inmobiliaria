b91a6aa2cf50bb88ae9d68966f4f5d7ca938e885
QuantumInformatik <juanguillermoalarcon@gmail.com>
2018-01-30 16:28:05 -0500

    Usamos el campos estado de la tabla login

--- js/main.js
+++ js/main.js
@@ -10,14 +10,17 @@ $(document).ready(function(){
 				success: function(json) {
 					if(json[0].sesion == "1"){
 						alert("Bienvenido admin: " + json[0].nom);
-	            		// $('input[type=text]').focus();
-	            		// setTimeout(function(){
-	            	    // $('input[type=text]').val('');
-	            	    // $('input[type=password]').val('');
-						// },600);		
-						location.href='entra.php';	
+						location.href='entra';	
 					}else if(json[0].sesion == "0"){
-						alert( "Usuario no encontrado.");	
+						alert("Por favor: <br> No intente entrar si no cuenta con un usuario y contraseña validos.");	
+	            		$('input[type=text]').focus();
+	            		setTimeout(function(){
+	            	    $('input[type=text]').val('');
+	            	    $('input[type=password]').val('');
+	            		},600);	
+
+					}else if(json[0].sesion == "3"){
+						alert("ADVERTENCIA: ya has iniciado sesion.");	
 	            		$('input[type=text]').focus();
 	            		setTimeout(function(){
 	            	    $('input[type=text]').val('');
--- piechpi/login.php
+++ piechpi/login.php
@@ -10,16 +10,18 @@ class Login{
         try{
             if(!empty($_POST['username']) and !empty($_POST['password'])){
                 $bd = new Conexion();
-                // if($bd==true){
-                    $this->user= $bd->quote($_POST['username']);
-                    $this->pass = $bd->quote($_POST['password']);
-                    $sql = $bd->prepare("SELECT * FROM tblusuarios WHERE usuarioPASS = $this->pass && usuarioUS = $this->user ");
-                    $sql->setFetchMode(PDO::FETCH_OBJ);
-                    $sql->execute();
-                    $datos=$sql->fetchALL();
-                    $cantidad = count($datos);
-                
-                    if( $cantidad > 0){
+                $this->user= $bd->quote($_POST['username']);
+                $this->pass = $bd->quote($_POST['password']);
+                $sql = $bd->prepare("SELECT * FROM tblusuarios WHERE usuarioPASS = $this->pass && usuarioUS = $this->user ");
+                $sql->setFetchMode(PDO::FETCH_OBJ);
+                $sql->execute();
+                $datos=$sql->fetchALL();
+                $cantidad = count($datos); 
+            
+                if( $cantidad > 0){
+                    if($datos[0]->usuarioEST != 1){
+                        $newEst = $datos[0]->usuarioID;
+                        $bd->query("UPDATE tblusuarios SET usuarioEST=1 WHERE usuarioID='$newEst' ");
                         $_SESSION["id"]=$datos[0]->usuarioID;
                         $_SESSION["user"]=$datos[0]->usuarioUS;
                         $_SESSION["pass"]=$datos[0]->usuarioPASS;
@@ -27,18 +29,12 @@ class Login{
                         $_SESSION["est"]=$datos[0]->usuarioEST; 
                         $datosJson[] = array('user'=>$_SESSION["user"],'pass' =>$_SESSION["pass"],'nom' =>$_SESSION["nom"],'est' =>$_SESSION["est"],'sesion'=>'1');
                     }else{
-                        $datosJson[] = array('sesion'=>'0');
-                    }
-                    echo json_encode($datosJson);
-
-                // }else{
-                //     $errorJson[]=array('sesion'=>'3', 'error'=>'No data base');
-                // $objJson= json_encode($errorJson);
-                // echo $objJson; 
-
-                // }
-                
-              
+                        $datosJson[]=array('sesion'=>'3');
+                    } 
+                }else{
+                    $datosJson[] = array('sesion'=>'0');
+                }
+                echo json_encode($datosJson);           
             }else{
                 // throw new Exception('Error campos vacios.');
                 $errorJson[]=array('sesion'=>'2', 'error'=>'Campos vacios.');
@@ -56,17 +52,20 @@ class Login{
         session_start();
         if(empty($_SESSION['user']) OR empty($_SESSION['pass'])){ 
             session_destroy();	
-            header("location:index.php"); 
+            header("location:index"); 
         } 
         $usuario=$_SESSION['nom'];
         echo "Bienvenido admin: " . $usuario;
-
     }
 
     public function cerrarAdmin(){
+        $bd  =  new Conexion();
+        session_start();
+        $newEst = $_SESSION['id'];
+        $bd->query("UPDATE tblusuarios SET usuarioEST=0 WHERE usuarioID='$newEst' ");
         unset($_SESSION['id'],$_SESSION['user'],$_SESSION['pass'],$_SESSION['nom'],$_SESSION['est']);
         session_destroy();
-        header("location:../index.php");
+        header("location:../index");
     }
 
 }
